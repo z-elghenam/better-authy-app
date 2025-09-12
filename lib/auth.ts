45af65750130b918da3daca3888ec9ccb62f2a06
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-09-12 14:50:56 +0000

    feat: implement authentication logic with password hashing and verification

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
+import { nextCookies } from "better-auth/next-js";
+import { hashPassword, verifyPasswordHash } from "./password";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -8,5 +10,17 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+    minPasswordLength: 8,
+    autoSignIn: false,
+    password: {
+      hash: hashPassword,
+      verify: verifyPasswordHash,
+    },
   },
+  advanced: {
+    database: {
+      generateId: false,
+    },
+  },
+  plugins: [nextCookies()],
 });
---
+++ lib/password.ts
@@ -0,0 +1,20 @@
+import { hash, verify } from "@node-rs/argon2";
+
+export async function hashPassword(password: string): Promise<string> {
+  return await hash(password, {
+    memoryCost: 19456,
+    timeCost: 2,
+    outputLen: 32,
+    parallelism: 1,
+  });
+}
+
+export async function verifyPasswordHash({
+  password,
+  hash,
+}: {
+  hash: string;
+  password: string;
+}): Promise<boolean> {
+  return await verify(hash, password);
+}
