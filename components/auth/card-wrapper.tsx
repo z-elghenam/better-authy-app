3aa6a5808a429f9bea75f0e9cd4e5971067bdb84
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-09-12 14:47:09 +0000

    feat: add CardWrapper component for improved UI structure and navigation feat: update package.json and package-lock.json with new dependencies for form handling and validation feat: add Zod schemas for login and registration validation

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,37 @@
+import { Card, CardContent, CardHeader, CardFooter } from "../ui/card";
+import { BackButton } from "./back-button";
+import { Header } from "./header";
+import { Social } from "./socail";
+
+interface CardWrapper {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
+}: CardWrapper) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter className="p-4 pt-0">
+          <Social />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton label={backButtonLabel} href={backButtonHref} />
+      </CardFooter>
+    </Card>
+  );
+};
---
+++ schemas/index.ts
@@ -0,0 +1,20 @@
+import * as z from "zod";
+
+export const loginSchema = z.object({
+  email: z.email({ message: "Email address is required" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+});
+
+export const registerSchema = z.object({
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+  email: z.email({
+    message: "Email address is required",
+  }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+});
