0833598cc3530c87222b9fa574c42597dcd51fb3
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-09-14 15:14:40 +0000

    feat: add admin page with user management and delete user functionality

---
+++ app/admin/page.tsx
@@ -0,0 +1,89 @@
+import {
+  DeleteUserButton,
+  PlaceholderDeleteUserButton,
+} from "@/components/delete-user-button";
+import { ReturnHomeButton } from "@/components/return-home-button";
+import { UserRoleSelect } from "@/components/user-role-select";
+import { auth } from "@/lib/auth";
+import { UserRole } from "@/lib/generated/prisma";
+import prisma from "@/lib/prisma";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function AdminPage() {
+  const headersList = await headers();
+
+  const session = await auth.api.getSession({
+    headers: headersList,
+  });
+
+  if (!session) redirect("/auth/login");
+
+  if (session.user.role !== "ADMIN") {
+    return (
+      <div className="px-8 py-16 container mx-auto max-w-screen-lg space-y-8">
+        <div className="space-y-4">
+          <ReturnHomeButton />
+
+          <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+
+          <p className="p-2 rounded-md text-lg bg-red-600 text-white font-bold">
+            FORBIDDEN
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  const users = await prisma.user.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  return (
+    <div className="px-8 py-16 container mx-auto max-w-screen-lg space-y-8">
+      <div className="space-y-4">
+        <ReturnHomeButton />
+
+        <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+
+        <p className="p-2 rounded-md text-lg bg-green-600 text-white font-bold">
+          ACCESS GRANTED
+        </p>
+      </div>
+
+      <div className="w-full overflow-x-auto">
+        <table className="table-auto min-w-full whitespace-nowrap">
+          <thead>
+            <tr className="border-b text-sm text-left">
+              <th className="px-4 py-2">ID</th>
+              <th className="px-4 py-2">Name</th>
+              <th className="px-4 py-2">Email</th>
+              <th className="px-4 py-2 text-center">Role</th>
+              <th className="px-4 py-2 text-center">Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id} className="border-b text-sm text-left">
+                <td className="px-4 py-2">{user.id.slice(0, 8)}</td>
+                <td className="px-4 py-2">{user.name}</td>
+                <td className="px-4 py-2">{user.email}</td>
+                <td className="px-4 py-2 text-center">{user.role}</td>
+                <td className="px-4 py-2 text-center">
+                  {user.role === "USER" ? (
+                    <DeleteUserButton userId={user.id} />
+                  ) : (
+                    <PlaceholderDeleteUserButton />
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
