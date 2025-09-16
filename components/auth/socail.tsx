ba331e7ed71f56478464e519f62462a9262431c1
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-09-16 11:56:25 +0000

    feat: refactor social sign-in buttons to use handleClick function for sign-in with google or github provider

--- components/auth/socail.tsx
+++ components/auth/socail.tsx
@@ -3,17 +3,32 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "../ui/button";
-import { useSearchParams } from "next/navigation";
+import { signIn } from "@/lib/auth-client";
 
 export const Social = () => {
-  const searchParams = useSearchParams();
+  const handleClick = async (provider: "google" | "github") =>
+    await signIn.social({
+      provider: provider,
+      callbackURL: "/profile",
+      errorCallbackURL: "/auth/login",
+    });
 
   return (
     <div className="flex items-center w-full gap-x-2">
-      <Button size="lg" className="flex-1" variant="outline">
+      <Button
+        size="lg"
+        className="flex-1"
+        variant="outline"
+        onClick={() => handleClick("google")}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button size="lg" className="flex-1" variant="outline">
+      <Button
+        size="lg"
+        className="flex-1"
+        variant="outline"
+        onClick={() => handleClick("github")}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
