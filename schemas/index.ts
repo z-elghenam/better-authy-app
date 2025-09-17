36bd55159dfa297f4ef19a707cf505272f72cd74
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-09-17 13:18:08 +0000

    feat: add resetPassword schema and infer the type of resetPassword

--- schemas/index.ts
+++ schemas/index.ts
@@ -24,3 +24,19 @@ export const emailSchema = z.object({
     message: "Email adress is required",
   }),
 });
+
+export const resetPasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" }),
+    confirmPassword: z.string().min(8, {
+      message: "Confirm password must be at least 8 characters long",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"], // error will appear under confirmPassword
+    message: "Passwords do not match",
+  });
+
+export type ResetPasswordValues = z.infer<typeof resetPasswordSchema>;
