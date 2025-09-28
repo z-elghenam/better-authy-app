361a30e50468d49c67746a83fdc62fdcbd440040
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-09-28 22:22:49 +0000

    feat: Add user profile dorpdown with logout
    - Implement UserButton with avatar and session-aware loading.
    - Add dropdown menu triggered by user avatar.
    - create reusable LogoutButton component.

---
+++ components/auth/user-button.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { FaUser } from "react-icons/fa";
+import { ExitIcon } from "@radix-ui/react-icons";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { LogoutButton } from "@/components/auth/logout-button";
+import { useSession } from "@/lib/auth-client";
+import { Loader2 } from "lucide-react";
+
+export const UserButton = () => {
+  const { data, isPending } = useSession();
+  const user = data?.user;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        {isPending ? (
+          <Loader2 size={15} className="animate-spin" />
+        ) : (
+          <Avatar>
+            <AvatarImage
+              src={
+                user?.image ||
+                "https://docs.material-tailwind.com/img/face-2.jpg"
+              }
+            />
+            <AvatarFallback className="bg-sky-500">
+              <FaUser className="text-white" />
+            </AvatarFallback>
+          </Avatar>
+        )}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40" align="end">
+        <LogoutButton>
+          <DropdownMenuItem>
+            <ExitIcon className="h-4 w-4 mr-2" />
+            Logout
+          </DropdownMenuItem>
+        </LogoutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
