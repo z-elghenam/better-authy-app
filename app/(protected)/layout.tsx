34d69122b90433fd89b444daa72f955aec6d2fd0
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-10-04 12:05:29 +0000

    feat: add auth protection to ProtectedLayout and pass user data to Navbar component

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,12 +1,21 @@
+import { headers } from "next/headers";
 import { Navbar } from "./_components/navbar";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 interface ProtectedLayoutProps {
   children: React.ReactNode;
 }
-function ProtectedLayout({ children }: ProtectedLayoutProps) {
+async function ProtectedLayout({ children }: ProtectedLayoutProps) {
+  const headersList = await headers();
+  const session = await auth.api.getSession({
+    headers: headersList,
+  });
+  if (!session) redirect("/auth/login");
+
   return (
     <div className="w-full min-h-screen flex flex-col gap-y-10 items-center justify-center bg-sky-600">
-      <Navbar />
+      <Navbar user={session.user} />
       {children}
     </div>
   );
