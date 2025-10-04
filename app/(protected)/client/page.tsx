4741cf76ec6ef6e659598348ad8697efaf7aa5d5
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-10-04 11:49:49 +0000

    refactor: simplify ClientPage with resuable component

--- app/(protected)/client/page.tsx
+++ app/(protected)/client/page.tsx
@@ -1,58 +1,17 @@
 "use client";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { LoadingCard } from "@/components/loading-card";
+import { UserInfo } from "@/components/user-info";
 import { useSession } from "@/lib/auth-client";
 import { redirect } from "next/navigation";
 
 export default function ClientPage() {
-  const { data, isPending } = useSession();
+  const { data: session, isPending } = useSession();
 
-  // While session is loading, don’t redirect yet
   if (isPending) {
-    return <p>Loading...</p>;
+    return <LoadingCard label="📱 Client components" />;
   }
 
-  // If no user after loading → redirect
-  if (!data) redirect("/auth/login");
+  if (!session) redirect("/auth/login");
 
-  const user = data.user;
-
-  return (
-    <Card className="w-[600px] shadow-md">
-      <CardHeader>
-        <p className="text-2xl font-semibold text-center">
-          📱 Client components
-        </p>
-      </CardHeader>
-      <CardContent className="space-y-4">
-        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-          <p className="text-sm font-medium">ID</p>
-          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 dark:bg-slate-800 rounded-md">
-            {user?.id}
-          </p>
-        </div>
-        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-          <p className="text-sm font-medium">Name</p>
-          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 dark:bg-slate-800 rounded-md">
-            {user?.name}
-          </p>
-        </div>
-        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-          <p className="text-sm font-medium">Email</p>
-          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 dark:bg-slate-800 rounded-md">
-            {user?.email}
-          </p>
-        </div>
-        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-          <p className="text-sm font-medium">Role</p>
-          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 dark:bg-slate-800 rounded-md">
-            {user?.role}
-          </p>
-        </div>
-
-        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-          <p className="text-sm font-medium">Two Factor Authentication</p>
-        </div>
-      </CardContent>
-    </Card>
-  );
+  return <UserInfo user={session.user} label="📱 Client components" />;
 }
